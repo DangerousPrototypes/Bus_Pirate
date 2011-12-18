da019b1c030fc126c347ddd70d8e28ba5c4ddba9
brentbxr <brentbxr@gmail.com>
2011-12-18 23:30:45 +0000

    Added eeReadByte() and eeWriteByte() for utilizing the EEPROM. BXR
    Usage:
    eeSetup()
    BP_EE_WP = 0;
    Perform read and writes now.

--- Firmware/onboardEEPROM.c
+++ Firmware/onboardEEPROM.c
@@ -1,3 +1,19 @@
+/*
+ * This file is part of the Bus Pirate project (http://code.google.com/p/the-bus-pirate/).
+ *
+ * Written and maintained by the Bus Pirate project.
+ *
+ * To the extent possible under law, the project has
+ * waived all copyright and related or neighboring rights to Bus Pirate. This
+ * work is published from United States.
+ *
+ * For details see: http://creativecommons.org/publicdomain/zero/1.0/.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ */
+
 #include "base.h"
 //#include "onboardEEPROM.h"
 
@@ -62,4 +78,52 @@ void eei2cSetup(void){
 	
 	// Enable I2C module
 	I2C1CONbits.I2CEN = 1;
+}
+
+/*
+void eei2cSetup(void);
+unsigned char eeread(void);
+void eewrite(unsigned char c);
+void eesendack(unsigned char ack);
+unsigned char eegetack(void);
+void eestop(void);
+void eestop(void);
+void eestart(void);
+unsigned char eetest(void); */
+
+unsigned char eeReadByte(unsigned long wAddr)
+{
+	char RetVal;
+
+	eestart();
+	eewrite(BP_EEPROM_ID_W);
+	eewrite((wAddr >> 8));		//Sent write Address high bits first
+	eewrite(wAddr);				//Send write address low bits section
+
+	eestart();
+	eewrite(BP_EEPROM_ID_R);
+
+	RetVal = eeread();			//Write the single bit.
+	
+	eesendack(1);				//send nak?
+	eestop();
+
+	return RetVal;
+}
+
+unsigned int eeWriteByte(char wByte, unsigned long wAddr)
+{
+	char RetVal;
+	eestart();
+	eewrite(BP_EEPROM_ID_W);
+	//test1=eegetack();
+	
+	eewrite((wAddr >> 8));		//Sent write Address high bits first
+	eewrite(wAddr);				//Send write address low bits section
+	//eegetack();
+	eewrite(wByte);		//Write the single bit.
+	RetVal = eegetack();
+	eestop(); 
+
+	return RetVal;
 }
--- Firmware/onboardEEPROM.h
+++ Firmware/onboardEEPROM.h
@@ -1,3 +1,25 @@
+/*
+ * This file is part of the Bus Pirate project (http://code.google.com/p/the-bus-pirate/).
+ *
+ * Written and maintained by the Bus Pirate project.
+ *
+ * To the extent possible under law, the project has
+ * waived all copyright and related or neighboring rights to Bus Pirate. This
+ * work is published from United States.
+ *
+ * For details see: http://creativecommons.org/publicdomain/zero/1.0/.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ */
+ 
+
+// Addressing the BP eeprom
+#define BP_EEPROM_ID 0xA0
+#define BP_EEPROM_ID_W 0xA0
+#define BP_EEPROM_ID_R 0xA1
+
 void eei2cSetup(void);
 unsigned char eeread(void);
 void eewrite(unsigned char c);
@@ -7,3 +29,6 @@ void eestop(void);
 void eestop(void);
 void eestart(void);
 unsigned char eetest(void);
+
+unsigned char eeReadByte(unsigned long wAddr);
+unsigned int eeWriteByte(char wByte, unsigned long wAddr);
