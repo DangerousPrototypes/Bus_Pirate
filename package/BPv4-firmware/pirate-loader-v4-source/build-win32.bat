823b54ef8f88be703ffe3c2038761ec4a9421faf
ianlesnet <ianlesnet@gmail.com>
2012-01-04 14:12:33 +0000

    Bus Pirate firmware v6 final

---
+++ package/BPv4-firmware/pirate-loader-v4-source/build-win32.bat
@@ -0,0 +1,9 @@
+@echo "Building for WINDOWS"
+
+@cl source/pirate-loader.c /DWIN32=1 /DNDEBUG=1 /D_CRT_SECURE_NO_DEPRECATE=1 >out.log 2>err.log || exit
+
+@del pirate-loader.obj
+@del out.log
+@del err.log
+
+pause
