aba992f5aa5c88d56d8ba2ebf310c1c5b0083855
taylan_ayken@yahoo.com <taylan_ayken@yahoo.com@0af3c9b6-adb7-11de-b66c-2b8a8bb3ca59>
2012-01-15 05:54:35 +0000

    Added a new code library to support new PIC programming adapter for BPv4.

--- trunk/Bus_Pirate/Firmware/procMenu.c
+++ trunk/Bus_Pirate/Firmware/procMenu.c
@@ -23,6 +23,9 @@
 #include "binIO.h"
 #include "SUMP.h"
 #include "basic.h"
+#ifdef BUSPIRATEV4
+#include "smps.h"
+#endif
 
 extern struct _bpConfig bpConfig;
 extern struct _modeConfig modeConfig;
@@ -538,6 +541,8 @@ end:			case 0x05: // ^E (goto end of line)
                 case 'K': modeConfig.altAUX = 3;
                     //bpWline("AUX2 selected");
                     BPMSG1264;
+                    break;
+				case 'M': bpSMPS();
                     break;
 #endif
                 case 'L': //bpWline("-bit order set (MSB)");
